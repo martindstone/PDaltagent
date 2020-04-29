40c470be7e6012caed317683d9e496a7e4301151
Martin Stone <martindstone@me.com>
2020-04-29 12:31:36 -0400

    chain webhooks for the same incident to preserve order

--- pdaltagent/tasks.py
+++ pdaltagent/tasks.py
@@ -7,38 +7,65 @@ import sqlite3
 from requests import HTTPError
 from pdaltagent.config import app
 from celery.utils.log import get_task_logger
+from celery import chain
 
-token = os.environ.get("PD_API_TOKEN")
-webhook_dest_url = os.environ.get("WEBHOOK_DEST_URL")
-is_overview = 'false' if os.environ.get("GET_ALL_LOG_ENTRIES") and os.environ.get("GET_ALL_LOG_ENTRIES").lower != 'false' else 'true'
-polling_interval_seconds = 10
+PD_API_TOKEN = os.environ.get("PD_API_TOKEN")
+WEBHOOK_DEST_URL = os.environ.get("WEBHOOK_DEST_URL")
+IS_OVERVIEW = 'false' if os.environ.get("GET_ALL_LOG_ENTRIES") and os.environ.get("GET_ALL_LOG_ENTRIES").lower != 'false' else 'true'
+POLLING_INTERVAL_SECONDS = 10
+if os.environ.get("POLLING_INTERVAL_SECONDS"):
+    try:
+        POLLING_INTERVAL_SECONDS = int(os.environ.get("POLLING_INTERVAL_SECONDS"))
+    except:
+        pass
 
 # keep activity db rows for 30 days
-keep_activity_seconds = 30*24*60*60
+KEEP_ACTIVITY_SECONDS = 30*24*60*60
+if os.environ.get("KEEP_ACTIVITY_SECONDS"):
+    try:
+        KEEP_ACTIVITY_SECONDS = int(os.environ.get("KEEP_ACTIVITY_SECONDS"))
+    except:
+        pass
 
-try:
-    polling_interval_seconds = int(os.environ.get("POLLING_INTERVAL"))
-except:
-    pass
+@app.on_after_finalize.connect
+def check_activity_store(sender, **kwargs):
+    conn = sqlite3.connect('/tmp/activity_store.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+    c = conn.cursor()
+    c.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='log_entries'")
+    if c.fetchone()[0]==0:
+        print('Table does not exist. Creating...')
+        c.executescript("""
+            BEGIN TRANSACTION;
+            CREATE TABLE log_entries (
+                            id TEXT NOT NULL,
+                            created_at timestamp);
+            COMMIT;
+        """)
+
+    conn.commit()
+    conn.close()
 
 @app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
-    if not token:
+    print('setting periodic tasks')
+    if not PD_API_TOKEN:
         print(f"Can't get log entries because no token is set. Please set PD_API_TOKEN environment variable if you want to poll PD log entries")
         return
 
-    if not webhook_dest_url:
+    if not WEBHOOK_DEST_URL:
         print(f"Can't send webhooks because no destination URL is set. Please set WEBHOOK_DEST_URL environment variable if you want to send webhooks")
         return
 
-    sender.add_periodic_task(float(polling_interval_seconds), poll_pd_log_entries.s())
-    sender.add_periodic_task(30.0, clean_activity_store.s())
+    sender.add_periodic_task(float(POLLING_INTERVAL_SECONDS), poll_pd_log_entries.s())
+    sender.add_periodic_task(float(KEEP_ACTIVITY_SECONDS), clean_activity_store.s())
 
 @app.task(autoretry_for=(HTTPError,),
           retry_kwargs={'max_retries': 10},
           retry_backoff=15,
           retry_backoff_max=60*60*2)
-def send_to_pd(routing_key, payload):
+def send_to_pd(routing_key, payload, *args):
+    print(f"routing key is {routing_key}")
+    print(f"payload is {payload}")
     return (routing_key, pd.send_v2_event(routing_key, payload))
 
 @app.task(autoretry_for=(HTTPError,),
@@ -55,35 +82,47 @@ def poll_pd_log_entries():
     r = c.execute('select * from log_entries order by created_at desc limit 1')
     a = r.fetchone()
     now = datetime.datetime.utcnow()
-    last_poll = a[1] if a else (now - datetime.timedelta(seconds=polling_interval_seconds))
+    last_poll = a[1] if a else (now - datetime.timedelta(seconds=POLLING_INTERVAL_SECONDS))
 
     since = last_poll.replace(microsecond=0).isoformat()
     until = now.replace(microsecond=0).isoformat()
 
-    params = {'since': since, 'until': until, 'is_overview': is_overview}
-    iles = pd.fetch_log_entries(token=token, params=params)
+    params = {'since': since, 'until': until, 'is_overview': IS_OVERVIEW}
+    iles = pd.fetch_log_entries(token=PD_API_TOKEN, params=params)
+    iles.reverse()
     new_iles = []
     dups = 0
+    ile_chains = {}
     for ile in iles:
         ile_id = ile['id']
         r = c.execute("select count(*) from log_entries where id = ?", (ile_id,))
         if r.fetchone()[0]:
             dups += 1
             continue
+        incident_id = ile['incident']['id']
+        if not ile_chains.get(incident_id):
+            print(f"creating task chain for incident id {incident_id}")
+            ile_chains[incident_id] = []
+        print(f"adding to task chain for incident id {incident_id}")
+        sig = send_webhook.si(WEBHOOK_DEST_URL, pd.ile_to_webhook(ile))
+        ile_chains[incident_id].append(sig)
         created_at = datetime.datetime.fromisoformat(ile['created_at'].rstrip('Z'))
         new_iles.append((ile_id, created_at))
-        send_webhook.delay(webhook_dest_url, pd.ile_to_webhook(ile))
+
     c.executemany('insert into log_entries values (?, ?)', new_iles)
     conn.commit()
     c.close()
     conn.close()
+    for incident_id, ile_chain in ile_chains.items():
+        print(f"Enqueuing chain of {len(ile_chain)} log entries for {incident_id}")
+        chain(ile_chain).delay()
     return f"{len(iles)} fetched, {len(new_iles)} processed, {dups} duplicates (since {since})"
 
 @app.task()
 def clean_activity_store():
     conn = sqlite3.connect('/tmp/activity_store.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
     c = conn.cursor()
-    d = datetime.datetime.utcnow() - datetime.timedelta(seconds=keep_activity_seconds)
+    d = datetime.datetime.utcnow() - datetime.timedelta(seconds=KEEP_ACTIVITY_SECONDS)
     c.execute("delete from log_entries where created_at < ?", (d,))
     conn.commit()
     r = c.rowcount
@@ -92,4 +131,7 @@ def clean_activity_store():
     return f"{r} rows deleted"
 
 def consume():
-    app.worker_main(['worker', '-A', 'pdaltagent.tasks', '-E', '-l', 'info'])
+    app.worker_main(['worker', '-n', 'events', '-A', 'pdaltagent.tasks', '-Q', 'pd_events', '-E', '-l', 'info'])
+
+def poll():
+    app.worker_main(['worker', '-n', 'poller', '-A', 'pdaltagent.tasks', '-B', '-Q', 'pd_poller,pd_webhooks', '-E', '-l', 'info'])
